e0b48cdabc2eaa222cf04cc47650c58b45758a4f
Michel <michel@fr-phonix.fr>
2023-07-11 18:14:13 +0200

    ajout id "disable" for toast

--- public/js/cnt.js
+++ public/js/cnt.js
@@ -4,6 +4,7 @@ const toastBody = document.getElementById("toast-body")
 const toast = new bootstrap.Toast(toastElement, {delay: 5000})
 
 htmx.on('htmx:afterRequest', (e) => {
+    if (e.target.id === "disable") return;
     if (e.detail.failed) {
         let obj = JSON.parse(e.detail.xhr.response)
         toast.show()
